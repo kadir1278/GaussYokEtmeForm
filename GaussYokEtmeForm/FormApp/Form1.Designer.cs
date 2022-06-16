a498ff84591504781f5b4a683aafead9221fda3f
Kadir Ari <kadirdestek1@gmail.com>
2022-06-16 15:01:54 +0300

    güncelleme

---
+++ GaussYokEtmeForm/FormApp/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace FormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA_Bir = new System.Windows.Forms.TextBox();
+            this.txtB_Bir = new System.Windows.Forms.TextBox();
+            this.txtC_Bir = new System.Windows.Forms.TextBox();
+            this.txtCevap_Bir = new System.Windows.Forms.TextBox();
+            this.txtCevap_Iki = new System.Windows.Forms.TextBox();
+            this.txtC_Iki = new System.Windows.Forms.TextBox();
+            this.txtB_Iki = new System.Windows.Forms.TextBox();
+            this.txtA_Iki = new System.Windows.Forms.TextBox();
+            this.txtCevap_Uc = new System.Windows.Forms.TextBox();
+            this.txtC_Uc = new System.Windows.Forms.TextBox();
+            this.txtB_Uc = new System.Windows.Forms.TextBox();
+            this.txtA_Uc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtError = new System.Windows.Forms.Label();
+            this.txtA_Dort = new System.Windows.Forms.TextBox();
+            this.txtB_Dort = new System.Windows.Forms.TextBox();
+            this.txtC_Dort = new System.Windows.Forms.TextBox();
+            this.txtCevap_Dort = new System.Windows.Forms.TextBox();
+            this.btnCevap = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA_Bir
+            // 
+            this.txtA_Bir.Location = new System.Drawing.Point(33, 176);
+            this.txtA_Bir.Name = "txtA_Bir";
+            this.txtA_Bir.Size = new System.Drawing.Size(100, 20);
+            this.txtA_Bir.TabIndex = 0;
+            // 
+            // txtB_Bir
+            // 
+            this.txtB_Bir.Location = new System.Drawing.Point(139, 176);
+            this.txtB_Bir.Name = "txtB_Bir";
+            this.txtB_Bir.Size = new System.Drawing.Size(100, 20);
+            this.txtB_Bir.TabIndex = 1;
+            // 
+            // txtC_Bir
+            // 
+            this.txtC_Bir.Location = new System.Drawing.Point(245, 176);
+            this.txtC_Bir.Name = "txtC_Bir";
+            this.txtC_Bir.Size = new System.Drawing.Size(100, 20);
+            this.txtC_Bir.TabIndex = 2;
+            // 
+            // txtCevap_Bir
+            // 
+            this.txtCevap_Bir.Location = new System.Drawing.Point(351, 176);
+            this.txtCevap_Bir.Name = "txtCevap_Bir";
+            this.txtCevap_Bir.Size = new System.Drawing.Size(100, 20);
+            this.txtCevap_Bir.TabIndex = 3;
+            // 
+            // txtCevap_Iki
+            // 
+            this.txtCevap_Iki.Location = new System.Drawing.Point(351, 202);
+            this.txtCevap_Iki.Name = "txtCevap_Iki";
+            this.txtCevap_Iki.Size = new System.Drawing.Size(100, 20);
+            this.txtCevap_Iki.TabIndex = 7;
+            // 
+            // txtC_Iki
+            // 
+            this.txtC_Iki.Location = new System.Drawing.Point(245, 202);
+            this.txtC_Iki.Name = "txtC_Iki";
+            this.txtC_Iki.Size = new System.Drawing.Size(100, 20);
+            this.txtC_Iki.TabIndex = 6;
+            // 
+            // txtB_Iki
+            // 
+            this.txtB_Iki.Location = new System.Drawing.Point(139, 202);
+            this.txtB_Iki.Name = "txtB_Iki";
+            this.txtB_Iki.Size = new System.Drawing.Size(100, 20);
+            this.txtB_Iki.TabIndex = 5;
+            // 
+            // txtA_Iki
+            // 
+            this.txtA_Iki.Location = new System.Drawing.Point(33, 202);
+            this.txtA_Iki.Name = "txtA_Iki";
+            this.txtA_Iki.Size = new System.Drawing.Size(100, 20);
+            this.txtA_Iki.TabIndex = 4;
+            // 
+            // txtCevap_Uc
+            // 
+            this.txtCevap_Uc.Location = new System.Drawing.Point(351, 228);
+            this.txtCevap_Uc.Name = "txtCevap_Uc";
+            this.txtCevap_Uc.Size = new System.Drawing.Size(100, 20);
+            this.txtCevap_Uc.TabIndex = 11;
+            // 
+            // txtC_Uc
+            // 
+            this.txtC_Uc.Location = new System.Drawing.Point(245, 228);
+            this.txtC_Uc.Name = "txtC_Uc";
+            this.txtC_Uc.Size = new System.Drawing.Size(100, 20);
+            this.txtC_Uc.TabIndex = 10;
+            // 
+            // txtB_Uc
+            // 
+            this.txtB_Uc.Location = new System.Drawing.Point(139, 228);
+            this.txtB_Uc.Name = "txtB_Uc";
+            this.txtB_Uc.Size = new System.Drawing.Size(100, 20);
+            this.txtB_Uc.TabIndex = 9;
+            // 
+            // txtA_Uc
+            // 
+            this.txtA_Uc.Location = new System.Drawing.Point(33, 228);
+            this.txtA_Uc.Name = "txtA_Uc";
+            this.txtA_Uc.Size = new System.Drawing.Size(100, 20);
+            this.txtA_Uc.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "A Degeri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "B Degeri";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "C Degeri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(348, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Cevap";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(569, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(463, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "A";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(572, 176);
+            this.txtB.Name = "txtB";
+            this.txtB.ReadOnly = true;
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 18;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(466, 176);
+            this.txtA.Name = "txtA";
+            this.txtA.ReadOnly = true;
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 19;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(678, 176);
+            this.txtC.Name = "txtC";
+            this.txtC.ReadOnly = true;
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(675, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "C";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtError
+            // 
+            this.txtError.AutoSize = true;
+            this.txtError.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtError.Location = new System.Drawing.Point(29, 60);
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(0, 24);
+            this.txtError.TabIndex = 23;
+            // 
+            // txtA_Dort
+            // 
+            this.txtA_Dort.Location = new System.Drawing.Point(33, 329);
+            this.txtA_Dort.Name = "txtA_Dort";
+            this.txtA_Dort.Size = new System.Drawing.Size(100, 20);
+            this.txtA_Dort.TabIndex = 24;
+            // 
+            // txtB_Dort
+            // 
+            this.txtB_Dort.Location = new System.Drawing.Point(139, 329);
+            this.txtB_Dort.Name = "txtB_Dort";
+            this.txtB_Dort.Size = new System.Drawing.Size(100, 20);
+            this.txtB_Dort.TabIndex = 25;
+            // 
+            // txtC_Dort
+            // 
+            this.txtC_Dort.Location = new System.Drawing.Point(245, 329);
+            this.txtC_Dort.Name = "txtC_Dort";
+            this.txtC_Dort.Size = new System.Drawing.Size(100, 20);
+            this.txtC_Dort.TabIndex = 26;
+            // 
+            // txtCevap_Dort
+            // 
+            this.txtCevap_Dort.Location = new System.Drawing.Point(351, 329);
+            this.txtCevap_Dort.Name = "txtCevap_Dort";
+            this.txtCevap_Dort.ReadOnly = true;
+            this.txtCevap_Dort.Size = new System.Drawing.Size(100, 20);
+            this.txtCevap_Dort.TabIndex = 27;
+            // 
+            // btnCevap
+            // 
+            this.btnCevap.Location = new System.Drawing.Point(466, 329);
+            this.btnCevap.Name = "btnCevap";
+            this.btnCevap.Size = new System.Drawing.Size(312, 23);
+            this.btnCevap.TabIndex = 28;
+            this.btnCevap.Text = "Cevap Bul";
+            this.btnCevap.UseVisualStyleBackColor = true;
+            this.btnCevap.Click += new System.EventHandler(this.btnCevap_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(33, 355);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(745, 23);
+            this.btnSave.TabIndex = 29;
+            this.btnSave.Text = "Veritabanına Kayıt Et";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCevap);
+            this.Controls.Add(this.txtCevap_Dort);
+            this.Controls.Add(this.txtC_Dort);
+            this.Controls.Add(this.txtB_Dort);
+            this.Controls.Add(this.txtA_Dort);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCevap_Uc);
+            this.Controls.Add(this.txtC_Uc);
+            this.Controls.Add(this.txtB_Uc);
+            this.Controls.Add(this.txtA_Uc);
+            this.Controls.Add(this.txtCevap_Iki);
+            this.Controls.Add(this.txtC_Iki);
+            this.Controls.Add(this.txtB_Iki);
+            this.Controls.Add(this.txtA_Iki);
+            this.Controls.Add(this.txtCevap_Bir);
+            this.Controls.Add(this.txtC_Bir);
+            this.Controls.Add(this.txtB_Bir);
+            this.Controls.Add(this.txtA_Bir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA_Bir;
+        private System.Windows.Forms.TextBox txtB_Bir;
+        private System.Windows.Forms.TextBox txtC_Bir;
+        private System.Windows.Forms.TextBox txtCevap_Bir;
+        private System.Windows.Forms.TextBox txtCevap_Iki;
+        private System.Windows.Forms.TextBox txtC_Iki;
+        private System.Windows.Forms.TextBox txtB_Iki;
+        private System.Windows.Forms.TextBox txtA_Iki;
+        private System.Windows.Forms.TextBox txtCevap_Uc;
+        private System.Windows.Forms.TextBox txtC_Uc;
+        private System.Windows.Forms.TextBox txtB_Uc;
+        private System.Windows.Forms.TextBox txtA_Uc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txtError;
+        private System.Windows.Forms.TextBox txtA_Dort;
+        private System.Windows.Forms.TextBox txtB_Dort;
+        private System.Windows.Forms.TextBox txtC_Dort;
+        private System.Windows.Forms.TextBox txtCevap_Dort;
+        private System.Windows.Forms.Button btnCevap;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ GaussYokEtmeForm/FormApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace FormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double FirstA = Convert.ToDouble(txtA_Bir.Text);
+                double SecondA = Convert.ToDouble(txtA_Iki.Text);
+                double ThirdA = Convert.ToDouble(txtA_Uc.Text);
+                double Answer1 = Convert.ToDouble(txtCevap_Bir.Text);
+
+                double FirstB = Convert.ToDouble(txtB_Bir.Text);
+                double SecondB = Convert.ToDouble(txtB_Iki.Text);
+                double ThirdB = Convert.ToDouble(txtB_Uc.Text);
+                double Answer2 = Convert.ToDouble(txtCevap_Iki.Text);
+
+                double FirstC = Convert.ToDouble(txtC_Bir.Text);
+                double SecondC = Convert.ToDouble(txtC_Iki.Text);
+                double ThirdC = Convert.ToDouble(txtC_Uc.Text);
+                double Answer3 = Convert.ToDouble(txtCevap_Uc.Text);
+
+
+                double[,] matris = new double[3, 3];
+                double[] deger = new double[3];
+
+
+                matris[0, 0] = FirstA;
+                matris[0, 1] = FirstB;
+                matris[0, 2] = FirstC;
+
+                matris[1, 0] = SecondA;
+                matris[1, 1] = SecondB;
+                matris[1, 2] = SecondC;
+
+                matris[2, 0] = ThirdA;
+                matris[2, 1] = ThirdB;
+                matris[2, 2] = ThirdC;
+
+                deger[0] = Answer1;
+                deger[1] = Answer2;
+                deger[2] = Answer3;
+
+
+
+                var sonuclar = GaussEleme(matris, deger);
+
+
+                double hava = 0;
+                double kahvalti = 0;
+                double borsa = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == 0)
+                        hava = sonuclar[i];
+                    else if (i == 1)
+                        kahvalti = sonuclar[i];
+                    else if (i == 2)
+                        borsa = sonuclar[i];
+                }
+
+                txtA.Text = hava.ToString();
+                txtB.Text = kahvalti.ToString();
+                txtC.Text = borsa.ToString();
+                txtError.Text = "";
+            }
+            catch (Exception)
+            {
+                txtError.Text = "Tüm Alanları Doldurunuz";
+            }
+        }
+
+        static double[] GaussEleme(double[,] matris, double[] deger)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                double geciciIlk = matris[i, i];
+                for (int k = 0; k < 3; k++)
+                {
+                    matris[i, k] /= geciciIlk;
+                }
+                deger[i] /= geciciIlk;
+
+
+                for (int j = i + 1; j < 3; j++)
+                {
+                    double carpim = matris[j, i] / matris[i, i];
+
+                    for (int l = 0; l < 3; l++)
+                    {
+                        matris[j, l] = matris[j, l] - (carpim * matris[i, l]);
+                    }
+                    deger[j] = deger[j] - (carpim * deger[i]);
+                }
+            }
+
+            double[] sonuclar = new double[3];
+            sonuclar[3 - 1] = deger[3 - 1];
+
+            for (int i = 3 - 2; i >= 0; i--)
+            {
+                double toplam = 0;
+                for (int j = i + 1; j < 3; j++)
+                {
+                    toplam += matris[i, j] * sonuclar[j];
+                }
+                sonuclar[i] = deger[i] - toplam;
+            }
+
+            return sonuclar;
+        }
+
+        private void btnCevap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double A, B, C;
+                A = Convert.ToDouble(txtA_Dort.Text) * Convert.ToDouble(txtA.Text);
+                B = Convert.ToDouble(txtB_Dort.Text) * Convert.ToDouble(txtB.Text);
+                C = Convert.ToDouble(txtC_Dort.Text) * Convert.ToDouble(txtC.Text);
+
+                txtCevap_Dort.Text = (A + B + C).ToString();
+            }
+            catch (Exception)
+            {
+                txtError.Text = "Cevap Hesaplanamadı. ";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            OleDbConnection baglanti;
+            OleDbCommand komut;
+            OleDbDataAdapter da;
+            string sorgu = "INSERT INTO GaussYokEtme(Gun,A Degeri,B Degeri,C Degeri,Cevap) values (@gun,@adeger,@bdeger,@cdeger,@cevap)";
+            baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=GaussYokEtme.accdb");
+            komut = new OleDbCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@gun", "1");
+            komut.Parameters.AddWithValue("@adeger", txtA_Bir.Text);
+            komut.Parameters.AddWithValue("@bdeger", txtB_Bir.Text);
+            komut.Parameters.AddWithValue("@cdeger", txtC_Bir.Text);
+            komut.Parameters.AddWithValue("@cevap", txtCevap_Bir.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+        }
+    }
+}
